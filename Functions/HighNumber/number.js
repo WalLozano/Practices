6c1e4c98ffdaf9999f65d8e204b2dabfa85d2288
WalLozano <125413661+WalLozano@users.noreply.github.com>
2023-08-18 19:25:00 +0200

    Little update

--- Functions/HighNumber/number.js
+++ Functions/HighNumber/number.js
@@ -2,8 +2,23 @@
 
 // Request two numbers and compare them to see who is higher, lower or equal 
 
-let num1 = prompt("First number!");
-let num2 = prompt("second Number!");
+
+
+let num1 = 0;
+let num2 = 0;
+
+// Cheking if is a number and request again the promp if the character is a letter
+
+do {
+    num1 = prompt("Fisrt number");
+} while  (isNaN(Number(num1)))
+
+
+do {
+    num2 = prompt("Second Number");
+} while  (isNaN(Number(num2)))
+
+
 
 function compare() {
     if(num1 > num2){
