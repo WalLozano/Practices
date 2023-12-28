841921ce340b991ebfa6a14ba5920e71cb27739d
WalLozano <125413661+WalLozano@users.noreply.github.com>
2023-12-28 04:45:39 -0800

    Update

---
+++ 5 Jumpealo/src/main.js
@@ -0,0 +1,10 @@
+const subirJump = document.getElementById('submit'); 
+
+
+subirJump.onclick = () => {
+    var surveyID = document.getElementById('surveyId').value;
+    var jumper = document.getElementById('jumper').value;
+        console.log(surveyID, jumper);
+}
+
+
